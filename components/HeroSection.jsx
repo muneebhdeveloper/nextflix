a6cbeced1d23c4c8b14887c04cd351f5bdf544c5
Muneeb Hussain Modi <muneebhussainmodi@gmail.com>
2022-01-23 00:45:20 +0500

    Separate HeroSection into its own component

---
+++ components/HeroSection.jsx
@@ -0,0 +1,62 @@
+import { Box, Heading, Text, Stack, Button, Icon } from "@chakra-ui/react";
+import { BsPlayFill, BsStopwatchFill } from "react-icons/bs";
+
+export default function HeroSection() {
+  return (
+    <Box
+      minHeight="80vh"
+      bgSize="cover"
+      bgRepeat="no-repeat"
+      bgImg="assets/img/MoviePoster.png"
+    >
+      <Stack maxW="8xl" margin="0px auto" height="80vh" justifyContent="center">
+        <Box>
+          <Stack>
+            <Heading
+              fontWeight={600}
+              color="brand.white"
+              fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
+              lineHeight={"110%"}
+            >
+              Watch your favourite shows <br />
+              <Text as={"span"} color={"brand.red"}>
+                at your space, your time
+              </Text>
+            </Heading>
+            <Text
+              paddingTop="15px"
+              maxW="4xl"
+              as={"span"}
+              fontSize="xl"
+              color={"brand.white"}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et
+              porta est. Nulla facilisi. Mauris in diam sed turpis euismod
+              iaculis. Mauris odio sem, convallis et massa id, mollis ornare
+              nisl. Etiam in placerat diam, consectetur porttitor nisi. Donec
+            </Text>
+            <Stack paddingTop="30px" direction="row">
+              <Button
+                py={8}
+                iconSpacing={5}
+                size="lg"
+                leftIcon={<Icon as={BsPlayFill} w={8} h={8} />}
+              >
+                Play Now
+              </Button>
+              <Button
+                py={8}
+                iconSpacing={5}
+                colorScheme="whiteAlpha"
+                size="lg"
+                leftIcon={<Icon as={BsStopwatchFill} w={8} h={8} />}
+              >
+                Watch Later
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Box>
+  );
+}
