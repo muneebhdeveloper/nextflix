18b0a72751eb7e6979a5a770335bd13af528364f
Muneeb Hussain Modi <muneebhussainmodi@gmail.com>
2022-01-22 22:49:23 +0500

    Working on navigation

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -16,7 +16,12 @@ import {
   Stack,
   Image,
 } from "@chakra-ui/react";
-import { HamburgerIcon, CloseIcon, Search2Icon } from "@chakra-ui/icons";
+import {
+  HamburgerIcon,
+  CloseIcon,
+  Search2Icon,
+  BellIcon,
+} from "@chakra-ui/icons";
 
 const Links = ["Home", "TV Shows", "Movies", "Recently Added", "My List"];
 
@@ -77,33 +82,48 @@ export default function Navigation() {
             </HStack>
           </HStack>
           <Flex alignItems={"center"}>
-            <IconButton
-              bg="transparent"
-              color="brand.white"
-              icon={<Search2Icon />}
-            />
-            <Menu>
-              <MenuButton
-                as={Button}
-                rounded={"full"}
-                variant={"link"}
-                cursor={"pointer"}
-                minW={0}
-              >
-                <Avatar
-                  size={"sm"}
-                  src={
-                    "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-                  }
-                />
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Link 1</MenuItem>
-                <MenuItem>Link 2</MenuItem>
-                <MenuDivider />
-                <MenuItem>Link 3</MenuItem>
-              </MenuList>
-            </Menu>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+              paddingLeft="35px"
+            >
+              <IconButton
+                size="lg"
+                bg="transparent"
+                colorScheme="whiteAlpha"
+                icon={<Search2Icon />}
+              />
+              <IconButton
+                size="lg"
+                bg="transparent"
+                colorScheme="whiteAlpha"
+                icon={<BellIcon />}
+              />
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={"full"}
+                  variant={"link"}
+                  cursor={"pointer"}
+                  minW={0}
+                >
+                  <Avatar
+                    size={"sm"}
+                    src={
+                      "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+                    }
+                  />
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>Link 1</MenuItem>
+                  <MenuItem>Link 2</MenuItem>
+                  <MenuDivider />
+                  <MenuItem>Link 3</MenuItem>
+                </MenuList>
+              </Menu>
+            </HStack>
           </Flex>
         </Flex>
 
